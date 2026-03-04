eb6ae557c1933d691d12e1d2e9da5420fe0069d6
dalmook <94118801+dalmook@users.noreply.github.com>
2026-03-05 08:32:53 +0900

    Update gocllm.py

--- gocllm.py
+++ gocllm.py
@@ -123,7 +123,7 @@ LLM_RETRY_ATTEMPTS = max(1, int(os.getenv("LLM_RETRY_ATTEMPTS", "3")))
 LLM_RETRY_BASE_DELAY = max(0.1, float(os.getenv("LLM_RETRY_BASE_DELAY", "1.5")))
 LLM_ALLOWED_USERS_SQL = os.getenv(
     "LLM_ALLOWED_USERS_SQL",
-    "select sso_id as senderKnoxId from user"
+    "SELECT SSO_ID FROM SCM_WP.T_T_FOR_MASTER A WHERE 1=1 AND a.sso_id = 'sungmook.cho' AND A.DEPT_NAME LIKE '%SCM%메모리%' and a.POSITION_CODE is not null AND A.SSO_ID NOT IN ('SCM.RPA','SCM 봇','메모리STO2','메모리 STO','dalbong.chatbot01', 'dalbongbot01', 'dalbong.bot01', 'command.center', 'thatcoolguy')"
 )
 LLM_ALLOWED_USERS_CACHE_TTL_SEC = max(0, int(os.getenv("LLM_ALLOWED_USERS_CACHE_TTL_SEC", "300")))
 
